8d9bb20fde36d8890c3c3a1975e4ba29620c7eb5
Roberta Capalbo <robertacapalbo@hotmail.com>
2023-02-09 13:49:07 -0300

    versão teste github

---
+++ src/components/Stories.js
@@ -0,0 +1,81 @@
+export default function Navbar() {
+    return (
+<div class="stories">
+<div class="story">
+  <div class="imagem">
+    <img src="assets\img\google.jpg" alt="9gag"/>
+  </div>
+  <div class="usuario">
+    google
+  </div>
+</div>
+
+<div class="story">
+  <div class="imagem">
+    <img src="assets\img\netflix.jpg" alt="meowed"/>
+  </div>
+  <div class="usuario">
+    netflix
+  </div>
+</div>
+
+<div class="story">
+  <div class="imagem">
+    <img src="assets\img\universalorlandoresorts.jpg" alt="barked"/>
+  </div>
+  <div class="usuario">
+    universalorlandoresorts
+  </div>
+</div>
+
+<div class="story">
+  <div class="imagem">
+    <img src="assets\img\waltdisneyworld.jpg" alt="nathanwpylestrangeplanet"/>
+  </div>
+  <div class="usuario">
+    waltdisneyworld
+  </div>
+</div>
+
+<div class="story">
+  <div class="imagem">
+    <img src="assets\img\harrypotterfilm.jpg" alt="wawawicomics"/>
+  </div>
+  <div class="usuario">
+    harrypotterfilm
+  </div>
+</div>
+
+<div class="story">
+  <div class="imagem">
+    <img src="assets\img\disney.jpg" alt="respondeai"/>
+  </div>
+  <div class="usuario">
+    disney
+  </div>
+</div>
+
+<div class="story">
+  <div class="imagem">
+    <img src="assets\img\Sephora.jpg" alt="filomoderna"/>
+  </div>
+  <div class="usuario">
+    sephora
+  </div>
+</div>
+
+<div class="story">
+  <div class="imagem">
+    <img src="assets\img\disneyparks.jpg" alt="memeriagourmet"/>
+  </div>
+  <div class="usuario">
+    disneyparks
+  </div>
+</div>
+
+<div class="setinha">
+  <ion-icon name="chevron-forward-circle"></ion-icon>
+</div>
+</div>
+    );
+}
---
+++ src/components/Usuario.js
@@ -0,0 +1,14 @@
+
+export default function Usuario() {
+    return (
+<div class="usuario">
+<img src="assets\img\Roberta.jpg" alt="imagem de perfil"/>
+<div class="texto">
+  <span>
+    <strong class="nomeUsuario">robertacapalbo</strong>
+    <ion-icon name="pencil"></ion-icon>
+  </span>
+</div>
+</div>
+    );
+}
