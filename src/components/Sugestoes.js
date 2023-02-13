efd752470d6c86d611dcad3f8dfda35ec07d6985
Roberta Capalbo <robertacapalbo@hotmail.com>
2023-02-13 13:54:45 -0300

    versão com usuário editável

--- src/components/Sugestoes.js
+++ src/components/Sugestoes.js
@@ -9,9 +9,9 @@ export default function Sugestoes() {
 
         <div className="sugestao">
           <div className="usuario">
-            <img src="assets/img/bad.vibes.memes.svg" alt="bad.vibes.memes.svg"/>
+            <img src="assets/img/dubaiIcon.jpg" alt="dubai"/>
             <div className="texto">
-              <div className="nome">bad.vibes.memes</div>
+              <div className="nome">dubai</div>
               <div className="razao">Segue você</div>
             </div>
           </div>
@@ -21,9 +21,9 @@ export default function Sugestoes() {
 
         <div className="sugestao">
           <div className="usuario">
-            <img src="assets/img/chibirdart.svg" alt="chibirdart"/>
+            <img src="assets/img/lvmh.jpg" alt="LVMH"/>
             <div className="texto">
-              <div className="nome">chibirdart</div>
+              <div className="nome">lvmh</div>
               <div className="razao">Segue você</div>
             </div>
           </div>
@@ -33,9 +33,9 @@ export default function Sugestoes() {
 
         <div className="sugestao">
           <div className="usuario">
-            <img src="assets/img/razoesparaacreditar.svg" alt="razoesparaacreditar" />
+            <img src="assets/img/amazon.jpg" alt="amazon" />
             <div className="texto">
-              <div className="nome">razoesparaacreditar</div>
+              <div className="nome">amazon</div>
               <div className="razao">Novo no Instagram</div>
             </div>
           </div>
@@ -45,9 +45,9 @@ export default function Sugestoes() {
 
         <div className="sugestao">
           <div className="usuario">
-            <img src="assets/img/adorable_animals.svg" alt="adorable_animals"/>
+            <img src="assets/img/meta.png" alt="meta"/>
             <div className="texto">
-              <div className="nome">adorable_animals</div>
+              <div className="nome">meta</div>
               <div className="razao">Segue você</div>
             </div>
           </div>
@@ -57,9 +57,9 @@ export default function Sugestoes() {
 
         <div className="sugestao">
           <div className="usuario">
-            <img src="assets/img/smallcutecats.svg" alt="smallcutecats"/>
+            <img src="assets/img/Microsoft.png" alt="microsoft''''''''''''''''''''''''''"/>
             <div className="texto">
-              <div className="nome">smallcutecats</div>
+              <div className="nome">microsoft</div>
               <div className="razao">Segue você</div>
             </div>
           </div>
--- src/components/Usuario.js
+++ src/components/Usuario.js
@@ -1,12 +1,36 @@
+import { useState } from "react"
 
 export default function Usuario() {
+  const [nome, setNome] = useState('robertacapalbo')
+  const [foto, setFoto] = useState("https://media.licdn.com/dms/image/C4E03AQFroDlXV_Bnag/profile-displayphoto-shrink_800_800/0/1656098104793?e=2147483647&v=beta&t=u962vCEch3zaNUc_HCG-6BtpwGCkclrLdPTEsvpEymk")
+
+  // let novaFoto = prompt("Insira o link da sua nova foto de perfil")
+  // let fotoPerfil = "assets\img\Roberta.jpg"
+  // fotoPerfil = !novaFoto ? "https://media.licdn.com/dms/image/C4E03AQFroDlXV_Bnag/profile-displayphoto-shrink_800_800/0/1656098104793?e=2147483647&v=beta&t=u962vCEch3zaNUc_HCG-6BtpwGCkclrLdPTEsvpEymk" : novaFoto
+
+  function escolherNome(){
+    const novoNome = prompt("Insira o seu nome")
+    if(!novoNome){
+       return
+    }
+    setNome(novoNome)
+  }
+
+  function escolherFoto(){
+    const novaFoto = prompt("Insira o link da foto de perfil que deseja utilizar")
+    if(!novaFoto){
+       return
+    }
+    setFoto(novaFoto)
+  }
+
     return (
 <div className="usuario">
-<img src="assets\img\Roberta.jpg" alt="imagem de perfil"/>
+<img onClick={escolherFoto} src={foto} alt="imagem de perfil"/>
 <div className="texto">
   <span>
-    <strong className="nomeUsuario">robertacapalbo</strong>
-    <ion-icon name="pencil"></ion-icon>
+    <strong className="nomeUsuario">{nome}</strong>
+    <ion-icon onClick={escolherNome} name="pencil"></ion-icon>
   </span>
 </div>
 </div>
